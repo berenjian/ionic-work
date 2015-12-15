a763756dcb9d6a6eef839f38182f26d2455d6100
Amirmahdi Berenjian <a.brenjian@1dws.com>
2015-12-15 16:21:27 +0330

    update controllers.js and app.js

--- ionBlank/www/js/app.js
+++ ionBlank/www/js/app.js
@@ -27,5 +27,51 @@ angular
 .config(function($stateProvider, $urlRouterProvider) {
  $stateProvider
 
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
 
+     .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+ 
+  $urlRouterProvider.otherwise('/tab/dash');
+ 
 };

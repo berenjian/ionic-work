067f3660fba2e18e11e72c6822e8af852046cec3
Amirmahdi Berenjian <a.brenjian@1dws.com>
2015-12-18 14:26:10 +0330

    add array

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -40,3 +40,15 @@ angular.module('starter.controllers', [])
     }, 1000);
   };
 })
+
+.controller('UsersListCtrl',function($scope){
+  $scope.users = [
+    { title: 'Amirmahdi', id: 1 },
+    { title: 'Ali', id: 2 },
+    { title: 'Hossein', id: 3 },
+    { title: 'Mohammad', id: 4 },
+  ];
+
+})
+.controller('UsersListCtrl', function($scope, $stateParams) {
+});
